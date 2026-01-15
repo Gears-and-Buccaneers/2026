197c0ead782ec73e7f4c5fb2a1624ead5793f8f4
Rami Mouro <ramimouro@gmail.com>
2026-01-15 10:02:12 -0700

    possible fix

--- autonomous/__init__.py
+++ autonomous/__init__.py
@@ -1,1 +1,15 @@
-"""All the auto modes."""
+"""All the auto modes.
+
+IMPORTANT: For MagicBot to discover auto modes, they must be imported here.
+MagicBot scans this package and looks for AutonomousStateMachine subclasses.
+"""
+
+# Import all autonomous mode classes so MagicBot can discover them
+from autonomous.choreo_examples import SimpleChoreoAuto
+from autonomous.just_leave import JustLeavePlease
+
+# Optionally list them for explicit exports
+__all__ = [
+    "JustLeavePlease",
+    "SimpleChoreoAuto",
+]
