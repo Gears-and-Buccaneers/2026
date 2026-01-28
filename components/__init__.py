622c280e71bd5ad925f124c89ed3b8d67356cf3c
Gavin Kistner <gavin@phrogz.net>
2026-01-27 22:28:32 -0700

    Add OperatorUSBGamepad; implement HSV logic for LEDs

--- components/__init__.py
+++ components/__init__.py
@@ -1,7 +1,7 @@
 """The base objects available when you `import components`."""
 
-from components.controllers import DriverController, DriverUSBGamepad, OperatorController
+from components.controllers import DriverController, DriverUSBGamepad, OperatorController, OperatorUSBGamepad
 from components.shooter import Shooter
 from components.swerve import Drivetrain
 
-__all__ = ["Drivetrain", "DriverController", "OperatorController", "DriverUSBGamepad", "Shooter"]
+__all__ = ["Drivetrain", "DriverController", "OperatorController", "DriverUSBGamepad", "OperatorUSBGamepad", "Shooter"]
